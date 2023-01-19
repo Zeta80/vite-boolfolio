352c0b4ec80c01107e52081f81cecbbd1608c4f6
Mirko Robbi <mirko.robbi@gmail.com>
2023-01-19 17:32:01 +0100

    add card with projects

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,27 @@
 <script >
-import axios from 'axios';
+import AppMain from './components/AppMain.vue';
+import AppHeader from './components/AppHeader.vue';
+
+
 
 export default {
   name: 'App',
-  data() {
-    return {
-      project: []
-    }
-  },
-  components: {
 
-  },
-  created() {
-    this.getProject();
-  },
-  methods: {
-    getProject() {
-      axios.get('http://127.0.0.1:8000/api/projects').then(resp => {
-        this.project = resp.data.result;
-      })
-    }
+  components: {
+    AppMain,
+    AppHeader
   }
+
 }
 </script>
 
 
 
 <template>
-
-  <h1>sono pagina Vite</h1>
-  <div class="container">
-    <h2>frontoffice</h2>
-  </div>
+  <AppHeader />
+  <main>
+    <AppMain />
+  </main>
 
 </template>
 
---
+++ src/components/AppMain.vue
@@ -0,0 +1,46 @@
+<script>
+import axios from 'axios';
+
+export default {
+    name: "AppMain",
+    data() {
+        return {
+            baseUrl: 'http://127.0.0.1:8000',
+            project: []
+        }
+    },
+    components: {
+
+    },
+    created() {
+        this.getProject();
+    },
+    methods: {
+        getProject() {
+            axios.get(`${this.baseUrl}/api/projects`).then(resp => {
+                this.project = resp.data.result;
+            })
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <h2 class="text-center">i miei Progetti</h2>
+        <div class="row justify-content-center">
+            <div class="col-10">
+                <div class="card" v-for="project in project" :key="project.id">
+                    <div class="card-header">
+                        Tech
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">{{ project.title }}</h5>
+                        <p class="card-text">{{ project.description }} </p>
+                        <a href="#" class="btn btn-primary">Scoprimi</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
